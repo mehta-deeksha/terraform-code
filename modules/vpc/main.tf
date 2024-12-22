086bbc823eab35edd921cdcd9c8d26835feb73d1
mehta-deeksha <111343664+mehta-deeksha@users.noreply.github.com>
2024-12-22 18:31:51 -0500

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,32 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Main-VPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zones[count.index]
+
+  tags = {
+    Name = "Public-Subnet-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "Private-Subnet-${count.index}"
+  }
+}
